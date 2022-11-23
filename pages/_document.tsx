3d7554f88ba4925156a5bae24d168275842ac21e
Frank Mayer <mail@frank-mayer.io>
2022-11-23 23:27:31 +0100

    fixed favicon link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,8 @@ const Document = () => {
   return (
     <Html lang="de-DE">
       <Head>
-        <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1590375905385922"
