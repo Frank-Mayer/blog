6ceace9202dbd75bbc0565d612de3880a2cbd00c
Frank-Mayer <mail@frank-mayer.io>
2022-11-26 23:47:44 +0100

    page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,42 @@ import "../styles/index.scss";
 import type { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Header />
+const variants = {
+  hidden: { opacity: 0, x: 100, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: -100, y: 0 },
+};
 
-    <main>
-      <Component {...pageProps} />
-    </main>
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
 
-    <Footer />
-  </>
-);
+  return (
+    <>
+      <Header />
+
+      <AnimatePresence
+        mode="wait"
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <motion.main
+          key={router.pathname}
+          variants={variants}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          transition={{ type: "linear", duration: 0.5 }}
+        >
+          <Component {...pageProps} />
+        </motion.main>
+      </AnimatePresence>
+
+      <Footer />
+    </>
+  );
+};
 
 export default App;
