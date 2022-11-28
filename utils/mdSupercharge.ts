57964f763aebcdef75ace6e9d55c7f48c2327563
Frank-Mayer <mail@frank-mayer.io>
2022-11-28 10:00:35 +0100

    added math

--- utils/mdSupercharge.ts
+++ utils/mdSupercharge.ts
@@ -1,8 +1,25 @@
-export const mdSupercharge = (md: string): string =>
-  md.replace(
-    /```([a-z]+)\r?\n([^`]+)\r?\n```/gi,
-    (_, lang, code) =>
-      `<SyntaxHighlighting language="${lang}">{` +
-      JSON.stringify(code) +
-      "}</SyntaxHighlighting>"
-  );
+export const mdSupercharge = (md: string): string => {
+  let tex = false;
+
+  const mdSupercharged = md
+    .replace(
+      /```([a-z]+)\r?\n([^`]+)\r?\n```/gi,
+      (_, lang, code) =>
+        `<SyntaxHighlighting language="${lang}">{` +
+        JSON.stringify(code) +
+        "}</SyntaxHighlighting>"
+    )
+    .replace(/[\r\n]{2,}\$([^\$\r\n]+)\$[\r\n]{2,}/g, (_, math) => {
+      tex = true;
+      return `<tex-math>{${JSON.stringify(math)}}</tex-math>`;
+    })
+    .replace(/\$([^\$\r\n]+)\$/g, (_, math) => {
+      tex = true;
+      return `<i-math>{${JSON.stringify(math)}}</i-math>`;
+    });
+
+  return tex
+    ? mdSupercharged +
+        '<script defer async src="https://cdn.jsdelivr.net/npm/tex-math@1.3.3/dist/tex-math.js"></script>'
+    : mdSupercharged;
+};
