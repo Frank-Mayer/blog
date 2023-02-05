71a413d334e867da4bc46a1e3a620e31c5157292
Frank-Mayer <git@frank-mayer.io>
2023-02-05 12:30:33 +0100

    sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+    site: "https://blog.frank-mayer.io/",
+    integrations: [
+        sitemap({
+            changefreq: "monthly",
+            lastmod: new Date(),
+        }),
+    ],
+});
